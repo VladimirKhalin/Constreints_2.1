a09e0e203b174aee1e73efd4edf8784b5dfaa986
VK <art.nebar@mail.ru>
2022-11-06 19:25:09 +0300

    Initial Commit

---
+++ Constreints_2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Constreints_2.1
+//
+//  Created by Vladimir Khalin on 06.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
